bc3c6fbb05445adc40122e12005d5aed35289f24
Lluppiter <petya1411@gmail.com>
2022-09-16 11:29:45 +0300

    add countryList & fix func

--- src/js/eventapi.js
+++ src/js/eventapi.js
@@ -1,22 +1,26 @@
 import axios from 'axios';
+import { countryCode } from './AllCountry';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 const API_KEY = 'lVBWF1m1jxEC3z9hVtA4qLnJqoOWHAKi';
 
 export class EventAPI {
   static size = 16;
   static page = 0;
-  static async searcEvent(data) {
+  static countryCode = 'US';
+  static async searcEvent(query) {
+    if (query.trim()) EventAPI.keyword = query;
     const config = {
       params: {
-        keyword: data,
+        keyword: query,
         size: EventAPI.size,
         page: EventAPI.page,
+        countryCode: EventAPI.countryCode,
         apikey: API_KEY,
       },
     };
     try {
       const response = await axios.get(BASE_URL, config);
-      return [response.data];
+      return response.data;
     } catch (error) {
       console.log(error.message);
     }

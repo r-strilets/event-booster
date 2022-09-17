92ff4e2779b5cc5dd47f5fb54fde6eb0c9428df6
Lluppiter <petya1411@gmail.com>
2022-09-18 02:50:11 +0300

    add fix I

--- src/js/paginationNumbers.js
+++ src/js/paginationNumbers.js
@@ -8,14 +8,14 @@ export function createPaginationMarcup(totalPages, currentPage) {
     maxPage = totalPages;
   }
   if (totalPages <= 6) {
-    for (i = 0; i < totalPages; i += 1) {
+    for (let i = 0; i < totalPages; i += 1) {
       elemLi += `<li class="pagination__item"><button  class='pagination__btn' value="${currentPage}" type="button"
        >${currentPage + 1}</button></li>`;
       currentPage += 1;
     }
   } else if (totalPages > 6 && maxPage === 50) {
     elemLi += `<li class="pagination__item"><button class='pagination__btn' value="0" type="button">1</button></li>`;
-    for (i = 1; i < 6; i += 1) {
+    for (let i = 1; i < 6; i += 1) {
       currentPage += 1;
       elemLi += `<li class="pagination__item"><button class='pagination__btn' value="${currentPage}" type="button">${
         currentPage + 1

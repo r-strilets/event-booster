05fb6bc03c5513814879d06aaad7f01622798ea2
Lluppiter <petya1411@gmail.com>
2022-09-17 20:29:32 +0300

    add fix

--- src/js/fetchEvents.js
+++ src/js/fetchEvents.js
@@ -4,6 +4,7 @@ export async function fetchEvents(data) {
   const events = await EventAPI.searcEvent(data);
   let eventsArray;
   try {
+    console.log(events);
     eventsArray = events._embedded.events;
   } catch (error) {
     window.alert('Жодного Івенту не знайдено');
--- src/js/mainModal.js
+++ src/js/mainModal.js
@@ -1,43 +1,8 @@
 export function createModal(response) {
-  return response.reduce(
-    (acc, { info, city, country, name, images, dates, url }) =>
-      acc +
-      ` <div class="event-card">
-     <button type="button" class="close__modal"><svg>
-  <use href="../images/sprite.svg#icon-close"></use>
-</svg></button>
-        <img
-          src=""
-          alt="photo"
-          loading="lazy"
-      <div class="info">
-        <p class="info-item">
-          <b>INFO</b>${info}
-        </p>
-        <p class="info-item">
-          <b>WHEN</b>${dates.localDate}<br>
-          ${dates.localTime}(${_embedded.venues.timeZone})
-        </p>
-        <p class="info-item">
-          <b>WHERE </b>${city},${country.name}<br>
-          ${_embedded.venues.name}
-        </p>
-        <p class="info-item">
-          <b>WHO </b>${name}
-        </p>
-        ${response.prices
-          .map(
-            price => `<p class="info-item">
-          <b>PRICES </b>${name}
-          <button><a href= "${price.url}">BUY TICKETS</a></button>
-        </p>`
-          )
-          .join('')}
-      </div>
-      <button class="info__button"><a href=" https://www.google.com/${name}">MORE FROM THIS AUTHOR</a></button>
-    </div> `,
-    ''
-  );
+  return response.reduce((acc, resp) => {
+    console.log(resp.name);
+    return acc + `<div>${resp.name}</div>`;
+  }, ``);
 }
 
 // const modalButton = document.querySelector('info__button');

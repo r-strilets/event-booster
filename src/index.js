a891ce7d1d69a7230d5cc4aabf91d8328bb41d41
Lluppiter <petya1411@gmail.com>
2022-09-20 15:17:03 +0300

    add commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import './js/createMarkup';
 import { EventAPI } from './js/eventapi';
 import { addCountryInSelectList } from './js/AllCountry';
 import { createModal } from './js/mainModal';
 import { countryCodes } from './js/AllCountry';
-import badRequestImg from './images/catSearch.svg';
 import './js/ourModal';
 import './js/button';
-import './js/paginationNumbers';
 import { fetchEvents } from './js/fetchEvents';
 import { createMarkup } from './js/createMarkup';
 import { createPaginationMarcup } from './js/paginationNumbers';
@@ -19,15 +16,19 @@ const form = document.querySelector('form');
 const formCountryInput = document.querySelector('#myCountry');
 const openMainModal = document.querySelector('.modal-backdrop');
 const closeMainModal = document.querySelector('.close__modal');
-
-// відслідковування пошукового інпуту
+const paginationList = document.querySelector('.pagination');
 const searchInput = document.querySelector('.form-input');
 let inputValue;
+let events;
+// перший виклик функції
+searcEventandCreateMarcup('');
+// Додавання країн до випадаючого списку
+addCountryInSelectList();
+// відслідковування пошукового інпуту
 searchInput.addEventListener('input', e => {
   inputValue = e.target.value;
 });
-
-let events;
+// функція пошуку події і створення розмітки
 async function searcEventandCreateMarcup(data) {
   const response = await fetchEvents(data);
   if (response) {
@@ -35,28 +36,14 @@ async function searcEventandCreateMarcup(data) {
     const totalPages = response.allData.page.totalPages;
     const currentPage = response.allData.page.number;
     paginationIteam.innerHTML = '';
+    EventAPI.page = 0;
     gallery.innerHTML = createMarkup(events);
     createPaginationMarcup(totalPages, currentPage);
     const currentBTN = document.querySelector(`button[value='${currentPage}']`);
-    try {
-      currentBTN.classList.add('pagination__btn--current');
-    } catch (error) {}
+    currentBTN.classList.add('pagination__btn--current');
   }
   formCountryInput.value = '';
 }
-// перший виклик функції
-searcEventandCreateMarcup('');
-// Додавання країн до випадаючого списку
-addCountryInSelectList();
-// Функція для зміни сторінки пошуку за допомогою пагінації
-const paginationList = document.querySelector('.pagination');
-paginationList.addEventListener('click', e => {
-  if (e.target.type === 'button') {
-    EventAPI.page = e.target.value;
-    searcEventandCreateMarcup(inputValue);
-  }
-});
-
 // Виклик для пошуку та рендеру карток за запитом у інпуті
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -64,29 +51,34 @@ form.addEventListener('submit', e => {
   searcEventandCreateMarcup(inputValue);
 });
 // Оновлення сторінки за країною
-function refreshCountry(e) {
+function refreshCountry(countryInput) {
   const country = countryCodes.filter(
-    request => request.name.toLowerCase() === e.target.value.toLowerCase()
+    query =>
+      query.name.toLowerCase() === countryInput.target.value.toLowerCase()
   );
-  try {
-    if (e.target.value) {
-      const countryCode = country[0].code;
-      EventAPI.countryCode = countryCode;
-    } else if (!e.target.value) {
-      EventAPI.countryCode = '';
-    }
-    searcEventandCreateMarcup(inputValue);
-  } catch (error) {
-    paginationIteam.innerHTML = '';
-    gallery.innerHTML = `<p class="header-title">Our cats did not find anything, please change your request or select another country</p><div class="bad--request"><img src="${badRequestImg}}" alt="Our cats did not find anything" width ="400px"/><a class="button" href="index.html">RETURN TO HOME</a></div>`;
+  if (countryInput.target.value) {
+    const countryCode = country[0].code;
+    EventAPI.countryCode = countryCode;
+  } else if (!countryInput.target.value) {
+    EventAPI.countryCode = '';
   }
+  searcEventandCreateMarcup(inputValue);
 }
+// виклик функції оновлення за країною.
 formCountryInput.addEventListener('input', _debounce(refreshCountry, 1000));
-
-gallery.addEventListener('click', e => {
-  e.preventDefault();
-  const eventCardID = e.target.closest('.gallery__item').id;
+// Функція для зміни сторінки пошуку за допомогою пагінації
+paginationList.addEventListener('click', evt => {
+  if (evt.target.type === 'button') {
+    EventAPI.page = evt.target.value;
+    searcEventandCreateMarcup(inputValue);
+  }
+});
+// рендер модального вікна по тицю на картку події
+gallery.addEventListener('click', card => {
+  card.preventDefault();
+  const eventCardID = card.target.closest('.gallery__item').id;
   const eventsID = events.filter(event => event.id === eventCardID);
+
   if (events) {
     openMainModal.removeAttribute('hidden');
     mainModal.innerHTML = createModal(eventsID);
@@ -102,14 +94,13 @@ gallery.addEventListener('click', e => {
     }
     openMainModal.addEventListener('click', onBackdropClick);
     function onBackdropClick(e) {
-      e.preventDefault();
       if (e.target === e.currentTarget) {
         bodyForBcdrop.classList.remove('no-scroll');
         openMainModal.setAttribute('hidden', 'true');
       }
     }
-    closeMainModal.addEventListener('click', e => {
-      e.preventDefault(), openMainModal.setAttribute('hidden', 'true');
+    closeMainModal.addEventListener('click', () => {
+      openMainModal.setAttribute('hidden', 'true');
       bodyForBcdrop.classList.remove('no-scroll');
     });
   }
--- src/js/AllCountry.js
+++ src/js/AllCountry.js
@@ -1,4 +1,5 @@
 export const countryCodes = [
+  { code: '', name: 'AllCountry' },
   { code: 'US', name: 'United States Of America' },
   { code: 'AD', name: 'Andorra' },
   { code: 'AI', name: 'Anguilla' },
@@ -63,7 +64,7 @@ export const countryCodes = [
   { code: 'PL', name: 'Poland' },
   { code: 'PT', name: 'Portugal' },
   { code: 'RO', name: 'Romania' },
-  { code: 'RU', name: 'Russian Federation' },
+  // { code: 'RU', name: 'Russian Federation' },
   { code: 'LC', name: 'Saint Lucia' },
   { code: 'SA', name: 'Saudi Arabia' },
   { code: 'RS', name: 'Serbia' },
